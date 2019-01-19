cebc6e8f98c7c326bab1ccb1ae31eca56136d729
soc33 <soc33@msn.com>
2019-01-19 14:42:02 -0500

    Sarah's javascript changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,18 +26,46 @@ $(document).ready(function () {
         $("#check-if-safe-later").css("display", "none");
 
     });
+    var x = document.getElementById("zipcode");
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+            "Longitude: " + position.coords.longitude;
+    }
+    $("#check-if-safe-now").on("click", function (e) {
+        e.preventDefault();
+        getLocation();
+        showPosition();
+
+
+    });
+    // https://api.opencagedata.com/geocode/v1/json?q=PLACENAME&key=278527ab562a439fb356e1ca002242fe
+
+
+    // https://api.opencagedata.com/geocode/v1/json?q=LAT+LNG&key=278527ab562a439fb356e1ca002242fe
+
+
     $("#check-if-safe-zipcode").on("click", function (e) {
         e.preventDefault();
         $(".laterContainer").css("display", "block");
         $(".nowContainer").css("display", "none");
-        var apiKey = "J4QW6QeT5JkCYP4vnCmUTpdF4";
-        var queryURL = "https://data.cityoforlando.net/resource/6qd7-sr7g.json?$$app_token=" + apiKey + "$where=date between '2015-01-10T12:00:00' and '2015-01-10T14:00:00'";
+        // var apiKey = "J4QW6QeT5JkCYP4vnCmUTpdF4";
+        var queryURL = "https://data.cityoforlando.net/resource/6qd7-sr7g.json?$where=case_date_time > '2017-11' and case_date_time < '2019-02' and case_offense_charge_type = 'Committed' and within_circle(location, 28.460357483, -81.458659246, 1000)";
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
+
+
         });
     });
 
@@ -47,7 +75,6 @@ $(document).ready(function () {
         database.ref().push({
             comment: comment
         });
-        // $(".comment-view").append(comment);
     });
 
     var zipCode = "";
@@ -83,49 +110,5 @@ $(document).ready(function () {
         var keyToDelete = $(this).attr("id");
         database.ref(keyToDelete).remove()
             .then($(this).closest('div').remove());
-    })
-
-    // Ajax call
-
-    // mapboxgl.accessToken = 'pk.eyJ1Ijoia2l0eWthdDMyNCIsImEiOiJjanIxOWE1cTgwaGNoM3p0Y2RjcjRpOWU1In0.ECL7UrpjDznPgiDynCKVmg';
-    // var map = new mapboxgl.Map({
-    //     container: 'map', // container id
-    //     style: 'mapbox://styles/mapbox/streets-v9',
-    //     center: [-96, 37.8], // starting position
-    //     zoom: 3 // starting zoom
-    // });
-
-    // // Add geolocate control to the map.
-    // map.addControl(new mapboxgl.GeolocateControl({
-    //     positionOptions: {
-    //         enableHighAccuracy: true
-    //     },
-    //     trackUserLocation: true
-    // }));
-
-    // map.addControl(new mapboxgl.AttributionControl({
-    //     compact: true
-    // }));
-    // $("#map").append(map);
-
-    // var options = {
-    //     enableHighAccuracy: true,
-    //     timeout: 5000,
-    //     maximumAge: 0
-    //   };
-
-    //   function success(pos) {
-    //     var crd = pos.coords;
-
-    //     console.log('Your current position is:');
-    //     console.log(`Latitude : ${crd.latitude}`);
-    //     console.log(`Longitude: ${crd.longitude}`);
-    //     console.log(`More or less ${crd.accuracy} meters.`);
-    //   }
-
-    //   function error(err) {
-    //     console.warn(`ERROR(${err.code}): ${err.message}`);
-    //   }
-
-    //   mapboxgl.getCurrentPosition(success, error, options);
+    });
 });
